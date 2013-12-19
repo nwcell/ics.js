6415ba58791da70b5f316bddc7564dd51a9fc354
OpenShift guest <kyle.hornberg@gmail.com>
2013-12-19 14:54:43 -0500

    Fix single digit months

--- ics.js
+++ ics.js
@@ -48,14 +48,14 @@ var ics = function() {
 			var end_date = new Date(stop);
 
 			var start_year = ('0000' + (start_date.getFullYear().toString())).slice(-4);
-			var start_month = ('00' + (start_date.getMonth().toString())).slice(-2);
+			var start_month = ('00' + ((start_date.getMonth() + 1).toString())).slice(-2);
 			var start_day = ('00' + (start_date.getDate().toString())).slice(-2);
-			var start = start_year + (Number(start_month)+1) + start_day;
+			var start = start_year + start_month + start_day;
 
 			var end_year = ('0000' + (end_date.getFullYear().toString())).slice(-4);
-			var end_month = ('00' + (end_date.getMonth().toString())).slice(-2);
+			var end_month = ('00' + ((end_date.getMonth() + 1).toString())).slice(-2);
 			var end_day = ('00' + (end_date.getDate().toString())).slice(-2);
-			var end = end_year + (Number(end_month)+1) + end_day;
+			var end = end_year + end_month + end_day;
 
 			var calendarEvent = [
 				'BEGIN:VEVENT',
--- test/spec/test.js
+++ test/spec/test.js
@@ -29,5 +29,17 @@
                 assert.strictEqual(cal.calendar(), 'BEGIN:VCALENDAR\nVERSION:2.0\nBEGIN:VEVENT\nCLASS:PUBLIC\nDESCRIPTION:Christian holiday celebrating the birth of Jesus Christ\nDTSTART;VALUE=DATE:20131225\nDTEND;VALUE=DATE:20131225\nLOCATION:Bethlehem\nSUMMARY;LANGUAGE=en-us:Christmas\nTRANSP:TRANSPARENT\nEND:VEVENT\nEND:VCALENDAR', 'calendar does not match');
             });
         });
+        describe('Calendar String of Single Digit Months', function () {
+            it('should have one event', function () {
+                cal.addEvent('Easter', 'Christian holiday celebrating the resurrection of Jesus Christ', 'Jerusalem', '04/20/2014', '04/20/2014');
+                assert.strictEqual(cal.events()[1], 'BEGIN:VEVENT\nCLASS:PUBLIC\nDESCRIPTION:Christian holiday celebrating the resurrection of Jesus Christ\nDTSTART;VALUE=DATE:20140420\nDTEND;VALUE=DATE:20140420\nLOCATION:Jerusalem\nSUMMARY;LANGUAGE=en-us:Easter\nTRANSP:TRANSPARENT\nEND:VEVENT');
+            });
+        });
+        describe('Calendar String of Single Digit Day', function () {
+            it('should have one event', function () {
+                cal.addEvent('April Fools Day', 'What isn\'t is', 'America', '4/1/2014', '4/1/2014');
+                assert.strictEqual(cal.events()[2], 'BEGIN:VEVENT\nCLASS:PUBLIC\nDESCRIPTION:What isn\'t is\nDTSTART;VALUE=DATE:20140401\nDTEND;VALUE=DATE:20140401\nLOCATION:America\nSUMMARY;LANGUAGE=en-us:April Fools Day\nTRANSP:TRANSPARENT\nEND:VEVENT');
+            });
+        });
     });
 })();
