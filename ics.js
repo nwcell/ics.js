4790ef4156bcecd620826dbcb30ace3698e8992d
Connor Bode <connor.bode@gmail.com>
2014-08-18 18:50:17 -0400

    fix lint

--- ics.js
+++ ics.js
@@ -51,7 +51,7 @@ var ics = function() {
                 typeof stop === 'undefined'
             ) {
                 return false;
-            };
+            }
 
             //TODO add time and time zone? use moment to format?
             var start_date = new Date(begin);
@@ -74,7 +74,7 @@ var ics = function() {
             // Since some calendars don't add 0 second events, we need to remove time if there is none...
             var start_time = '';
             var end_time = '';
-            if (start_minutes + start_seconds + end_minutes + end_seconds != 0) {
+            if (start_minutes + start_seconds + end_minutes + end_seconds !== 0) {
                 start_time = 'T' + start_hours + start_minutes + start_seconds;
                 end_time = 'T' + end_hours + end_minutes + end_seconds;
             }
