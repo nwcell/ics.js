0f11343c6fc9853685c68a602391326223ecba64
sacha.bertschi <sacha.bertschi@YOTSUBA>
2017-03-28 16:02:54 +0200

    Setting things up

---
+++ test/spec/test.js
@@ -0,0 +1,45 @@
+/*global describe, it, cal, assert */
+
+(function () {
+    'use strict';
+    describe('Load ics Object', function () {
+        describe('Calendar Events Array', function () {
+            it('should be an empty array initially', function () {
+                assert.isArray(cal.events(), 'calendarEvents not an array');
+                assert.lengthOf(cal.events(), '0');
+            });
+        });
+        describe('Calendar String', function () {
+            it('should be a string with no events', function () {
+                assert.strictEqual(cal.calendar(), 'BEGIN:VCALENDAR\r\nVERSION:2.0\r\n\r\nEND:VCALENDAR', 'calendar does not match');
+            });
+        });
+    });
+    describe('Add 1 Event', function () {
+        describe('Calendar Events Array', function () {
+            it('should have one event', function () {
+                cal.addEvent('Christmas', 'Christian holiday celebrating the birth of Jesus Christ', 'Bethlehem', '12/25/2013', '12/25/2013');
+                assert.isArray(cal.events(), 'calendarEvents not an array');
+                assert.lengthOf(cal.events(), '1');
+                assert.strictEqual(cal.events()[0], 'BEGIN:VEVENT\r\nCLASS:PUBLIC\r\nDESCRIPTION:Christian holiday celebrating the birth of Jesus Christ\r\nDTSTART;VALUE=DATE:20131225\r\nDTEND;VALUE=DATE:20131225\r\nLOCATION:Bethlehem\r\nSUMMARY;LANGUAGE=en-us:Christmas\r\nTRANSP:TRANSPARENT\r\nEND:VEVENT');
+            });
+        });
+        describe('Calendar String', function () {
+            it('should have one event', function () {
+                assert.strictEqual(cal.calendar(), 'BEGIN:VCALENDAR\r\nVERSION:2.0\r\nBEGIN:VEVENT\r\nCLASS:PUBLIC\r\nDESCRIPTION:Christian holiday celebrating the birth of Jesus Christ\r\nDTSTART;VALUE=DATE:20131225\r\nDTEND;VALUE=DATE:20131225\r\nLOCATION:Bethlehem\r\nSUMMARY;LANGUAGE=en-us:Christmas\r\nTRANSP:TRANSPARENT\r\nEND:VEVENT\r\nEND:VCALENDAR', 'calendar does not match');
+            });
+        });
+        describe('Calendar String of Single Digit Months', function () {
+            it('should have one event', function () {
+                cal.addEvent('Easter', 'Christian holiday celebrating the resurrection of Jesus Christ', 'Jerusalem', '04/20/2014', '04/20/2014');
+                assert.strictEqual(cal.events()[1], 'BEGIN:VEVENT\r\nCLASS:PUBLIC\r\nDESCRIPTION:Christian holiday celebrating the resurrection of Jesus Christ\r\nDTSTART;VALUE=DATE:20140420\r\nDTEND;VALUE=DATE:20140420\r\nLOCATION:Jerusalem\r\nSUMMARY;LANGUAGE=en-us:Easter\r\nTRANSP:TRANSPARENT\r\nEND:VEVENT');
+            });
+        });
+        describe('Calendar String of Single Digit Day', function () {
+            it('should have one event', function () {
+                cal.addEvent('April Fools Day', 'What isn\'t is', 'America', '4/1/2014', '4/1/2014');
+                assert.strictEqual(cal.events()[2], 'BEGIN:VEVENT\r\nCLASS:PUBLIC\r\nDESCRIPTION:What isn\'t is\r\nDTSTART;VALUE=DATE:20140401\r\nDTEND;VALUE=DATE:20140401\r\nLOCATION:America\r\nSUMMARY;LANGUAGE=en-us:April Fools Day\r\nTRANSP:TRANSPARENT\r\nEND:VEVENT');
+            });
+        });
+    });
+})();
